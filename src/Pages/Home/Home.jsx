3501f4edf10cdeb78c023d97aefd2a798aa2f953
Felipe Farnetti <felipefarnetti@MacBook-Air-de-Felipe.local>
2023-08-12 18:01:50 +0200

    ready

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,18 @@
 import "./home.css";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    // Update the URL without triggering a page reload
+    window.history.pushState({}, "", "/home");
+
+    // Cleanup function
+    return () => {
+      // Reset the URL when the component unmounts
+      window.history.pushState({}, "", "/");
+    };
+  }, []);
+
   return (
     <div className="home-background">
       <div className="home-overlay">
