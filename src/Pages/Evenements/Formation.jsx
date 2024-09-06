632d7e13ade4a72daf32a677c46d30d90e88b1ea
Felipe Farnetti <felipefarnetti@gmail.com>
2024-09-06 11:37:38 +0200

    avec calendar

--- src/Pages/Evenements/Formation.jsx
+++ src/Pages/Evenements/Formation.jsx
@@ -4,7 +4,6 @@ import "./formation.css";
 const Formation = () => {
   return (
     <div className="formation-container">
-      <h1>Google Calendar</h1>
       <iframe
         src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&bgcolor=%23ffffff&showPrint=0&showTabs=0&showTz=0&showCalendars=0&mode=WEEK&src=ZmVsaXBlZmFybmV0dGlAZ21haWwuY29t&color=%23E67C73"
         frameBorder="0"
