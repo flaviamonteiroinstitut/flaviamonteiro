f497d7b7be84b5b9d84ec5242168a523389d4dbc
Felipe Farnetti <felipefarnetti@gmail.com>
2024-09-06 11:35:28 +0200

    avec calendar

--- src/Pages/Evenements/Formation.jsx
+++ src/Pages/Evenements/Formation.jsx
@@ -9,7 +9,6 @@ const Formation = () => {
         src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&bgcolor=%23ffffff&showPrint=0&showTabs=0&showTz=0&showCalendars=0&mode=WEEK&src=ZmVsaXBlZmFybmV0dGlAZ21haWwuY29t&color=%23E67C73"
         frameBorder="0"
         scrolling="no"
-        title="Google Calendar"
       ></iframe>
     </div>
   );
