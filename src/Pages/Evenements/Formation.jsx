e73be2ccd2a3fa1b3b493234bdfcaf7415cf44fb
Felipe Farnetti <felipefarnetti@gmail.com>
2024-09-06 11:13:55 +0200

    avec calendar

--- src/Pages/Evenements/Formation.jsx
+++ src/Pages/Evenements/Formation.jsx
@@ -1,59 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { gapi } from "gapi-script";
-import "./formation.css"
+import React from "react";
+import "./formation.css";
 
-const formation = () => {
-  const [events, setEvents] = useState([]);
-
-  const CLIENT_ID = "475720912712-pv1mg95gif67dc7offrrc9d6q99lin0q.apps.googleusercontent.com";
-  const API_KEY = "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";
-  const CALENDAR_ID = "your_calendar_id@group.calendar.google.com";
-
-  useEffect(() => {
-    const initClient = () => {nù
-      gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
-        scope: "https://www.googleapis.com/auth/calendar.readonly",
-      }).then(() => {
-        gapi.client.load("calendar", "v3", () => {
-          listUpcomingEvents();
-        });
-      });
-    };
-
-    gapi.load("client:auth2", initClient);
-  }, []);
-
-  const listUpcomingEvents = () => {
-    gapi.client.calendar.events
-      .list({
-        calendarId: CALENDAR_ID,
-        timeMin: new Date().toISOString(),
-        showDeleted: false,
-        singleEvents: true,
-        maxResults: 10,
-        orderBy: "startTime",
-      })
-      .then((response) => {
-        const events = response.result.items;
-        setEvents(events);
-      });
-  };
-
+const Formation = () => {
   return (
-    <div classname="formation-container">
-      <h1>Upcoming Events</h1>
-      <ul>
-        {events.map((event) => (
-          <li key={event.id}>
-            {event.summary} ({new Date(event.start.dateTime).toLocaleString()})
-          </li>
-        ))}
-      </ul>
+    <div className="formation-container">
+      <h1>Google Calendar</h1>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&bgcolor=%23F6BF26&mode=WEEK&src=ZmVsaXBlZmFybmV0dGlAZ21haWwuY29t&color=%23E67C73"
+        frameBorder="0"
+        scrolling="no"
+        title="Google Calendar"
+      ></iframe>
     </div>
   );
 };
 
-export default formation;
+export default Formation;
